2b3fbba1a4326c622c2d2c322befe0aa0dfb6255
Tgb03 <tudortrandafir@gmail.com>
2026-07-03 10:04:47 +0200

    fixed more bugs

--- gtde-cmd/src/commands/new.rs
+++ gtde-cmd/src/commands/new.rs
@@ -3,10 +3,11 @@ use std::{fs, path::Path};
 use crate::{commands::init, error::Error};
 
 pub fn new<'a>(name: String, path: &'a Path) -> Result<(), Error> {
-    fs::create_dir_all(path.join(&name))
-        .map_err(Error::io_at(path.join(&name)))?;
+    let path = path.join(&name);
+    fs::create_dir_all(&path)
+        .map_err(Error::io_at(&path))?;
     
-    init::init(path)?;
+    init::init(&path)?;
 
     Ok(())
 }
---
+++ gtde-cmd/src/lib.rs
@@ -0,0 +1,7 @@
+pub mod args;
+pub mod commands;
+pub mod config;
+pub mod error;
+pub mod file_utils;
+pub mod loadable;
+pub mod manifest;
